4914eceaed334b02d61f4444f92572ed08eca422
Murilo Amorim Ferrreira <148826050+MuriloSuda01@users.noreply.github.com>
2025-03-19 16:44:28 -0300

    Add files via upload

---
+++ Tarefa_Wrappers/src/Murilo/Casting.java
@@ -0,0 +1,12 @@
+package Murilo;
+
+public class Casting {
+	public static void main(String[]args) {
+		int num3= 12345;
+		System.out.println(num3);
+		Long num1=(long) num3;
+		System.out.println(num1);
+		
+	}
+
+}
